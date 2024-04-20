07a387cbdf15b7258d058a913bc08dc1408d5bb6
Kristy Ho <kristy02ho@gmail.com>
2024-04-19 22:14:47 -0600

    added user and places schema

---
+++ server.js
@@ -0,0 +1,100 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var passport = require("passport");
+// var authController = require("./auth");
+var authJwtController = require("./auth_jwt");
+var jwt = require("jsonwebtoken");
+var cors = require("cors");
+var User = require("./Users");
+var Places = require("./Places");
+const mongoose = require("mongoose");
+
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(passport.initialize());
+
+var router = express.Router();
+
+router.post("/signup", function (req, res) {
+    if (!req.body.username || !req.body.password) {
+      res.json({
+        success: false,
+        msg: "Please include both username and password to signup.",
+      });
+    } else {
+      var user = new User();
+      user.name = req.body.name;
+      user.username = req.body.username;
+      user.password = req.body.password;
+      user.hobby = req.body.hobby;
+  
+      user.save(function (err) {
+        if (err) {
+          if (err.code == 11000)
+            return res.json({
+              success: false,
+              message: "A user with that username already exists.",
+            });
+          else return res.json(err);
+        }
+  
+        res.json({ success: true, msg: "Successfully created new user." });
+      });
+    }
+  });
+  
+  router.post("/signin", function (req, res) {
+    var userNew = new User();
+    userNew.username = req.body.username;
+    userNew.password = req.body.password;
+  
+    User.findOne({ username: userNew.username })
+      .select("name username password")
+      .exec(function (err, user) {
+        if (err) {
+          res.send(err);
+        }
+  
+        user.comparePassword(userNew.password, function (isMatch) {
+          if (isMatch) {
+            var userToken = { id: user.id, username: user.username };
+            var token = jwt.sign(userToken, process.env.SECRET_KEY);
+            res.json({ success: true, token: "JWT " + token });
+          } else {
+            res
+              .status(401)
+              .send({ success: false, msg: "Authentication failed." });
+          }
+        });
+      });
+
+  });
+
+  router
+  .route("/places")
+  .get((req, res) => {
+    Places.find(
+        {
+            city: { $exists: true, $ne: null },
+            country: { $exists: true, $ne: null },
+            continent: { $exists: true, $ne: null }
+        },
+        (err, places) => {
+            if (err) {
+                res.status(400).send(err);
+            } else {
+                res.status(200).json(places);
+            }
+        }
+    )
+})
+
+
+
+app.use("/", router);
+app.listen(process.env.PORT || 8080);
+module.exports = app; // for testing only
